b25ec37f216e954fa9bdb6b347e21e57582b8bd0
NanMed <46913776+NanMed@users.noreply.github.com>
2019-11-24 22:09:30 -0600

    Copilando altaRegistro

--- WEB-INF/classes/altaRegistro.java
+++ WEB-INF/classes/altaRegistro.java
@@ -2,8 +2,7 @@ import java.sql.*;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*; 
-import objetos.Cliente;
-import objetos.Cuenta;
+import objetos.Registro;
 import javax.servlet.annotation.WebServlet;
 
 @WebServlet("/altaRegistro")
@@ -39,7 +38,7 @@ public class altaRegistro extends HttpServlet{
             String omni_gse= request.getParameter("omni_gse");
             String dia= request.getParameter("dia");
 
-			Registro newRegistro = new Registro(eje1Levantamiento,intento1E1L,intento2E1L,eje1Velocidad,intento1E1V,intento2E1V,eje1Equilibrio,intento1E1E,intento2E1E,eje2Equilibrio,intento1E2E,intento2E2E,eje3Equilibrio,intento1E3E,intento2E3E,ritmoCInicial,ritmoCFinal,omni_gse,dia);
+			//Registro newRegistro = new Registro(eje1Levantamiento,intento1E1L,intento2E1L,eje1Velocidad,intento1E1V,intento2E1V,eje1Equilibrio,intento1E1E,intento2E1E,eje2Equilibrio,intento1E2E,intento2E2E,eje3Equilibrio,intento1E3E,intento2E3E,ritmoCInicial,ritmoCFinal,omni_gse,dia);
 
             
             Class.forName("com.mysql.jdbc.Driver");
@@ -53,7 +52,7 @@ public class altaRegistro extends HttpServlet{
             
             int window= Integer.parseInt(request.getParameter("pestana"));
 
-           String sql = "INSERT INTO registro values('"+ieje1Levantamiento+"', " + intento1E1L + ", " + intento2E1L + ", '"  + eje1Velocidad + "', "  + intento1E1V + ", "  + intento2E1V+ ", ''"  + eje1Equilibrio + "', "  + intento1E1E + " , "  + intento2E1E + ", '"  + eje2Equilibrio + "', "  + intento1E2E + " , "  + intento2E2E + ", '"  + eje3Equilibrio + "', "  + intento1E3E + " , "  + intento2E3E + ", '"  + ritmoCInicial + "', '"  + ritmoCFinal + "' , '"  + omni_gse + "',  '"  + dia + "');";
+            String sql = "INSERT INTO registro values('"+eje1Levantamiento+"', " + intento1E1L + ", " + intento2E1L + ", '"  + eje1Velocidad + "', "  + intento1E1V + ", "  + intento2E1V+ ", ''"  + eje1Equilibrio + "', "  + intento1E1E + " , "  + intento2E1E + ", '"  + eje2Equilibrio + "', "  + intento1E2E + " , "  + intento2E2E + ", '"  + eje3Equilibrio + "', "  + intento1E3E + " , "  + intento2E3E + ", '"  + ritmoCInicial + "', '"  + ritmoCFinal + "' , '"  + omni_gse + "',  '"  + dia + "');";
             stat.executeUpdate(sql);
 
           
@@ -89,4 +88,4 @@ public class altaRegistro extends HttpServlet{
 
 	
 
-}*/
+}
